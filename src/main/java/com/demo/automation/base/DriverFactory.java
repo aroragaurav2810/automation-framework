22d4c9a090faa66679bab515b8b6e78c0b3670f2
gaurav <gaurav.ar.arora@oracle.com>
2026-04-02 02:53:15 +0530

    Enabled headless mode for CI

--- src/main/java/com/demo/automation/base/DriverFactory.java
+++ src/main/java/com/demo/automation/base/DriverFactory.java
@@ -3,6 +3,7 @@ package com.demo.automation.base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
@@ -14,7 +15,14 @@ public class DriverFactory {
 
     public static void initDriver() {
         WebDriverManager.chromedriver().setup();
-        driver.set(new ChromeDriver());
+        ChromeOptions options = new ChromeOptions();
+
+        // 🔥 VERY IMPORTANT FOR CI
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver.set(new ChromeDriver(options));
     }
 
     public static void quitDriver() {
